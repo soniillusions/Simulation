a6fa43a0fec8542ab00bc8258d4225353e7ff6c1
Alexey Maryshenkov <alekseycrosso@gmail.com>
2026-04-04 00:36:33 +0300

    update Simulation

--- src/simulation/core/Simulation.java
+++ src/simulation/core/Simulation.java
@@ -1,5 +1,6 @@
 package simulation.core;
 
+import simulation.actions.MoveCreaturesAction;
 import simulation.actions.SpawnEntitiesAction;
 import simulation.renderer.ConsoleRenderer;
 
@@ -7,6 +8,7 @@ public class Simulation {
     private WorldMap worldMap;
     private SpawnEntitiesAction spawnEntitiesAction;
     private ConsoleRenderer consoleRenderer;
+    private MoveCreaturesAction moveCreaturesAction;
     private int width = 10;
     private int height = 10;
 
@@ -14,10 +16,29 @@ public class Simulation {
         this.worldMap = new WorldMap(width, height);
         this.spawnEntitiesAction = new SpawnEntitiesAction();
         this.consoleRenderer = new ConsoleRenderer();
+        this.moveCreaturesAction = new MoveCreaturesAction();
     }
 
     public void startSimulation() {
         spawnEntitiesAction.execute(worldMap);
         consoleRenderer.renderWorldState(worldMap);
+        System.out.println();
+        sleepSimulation();
+
+        while (true) {
+            moveCreaturesAction.execute(worldMap);
+            consoleRenderer.renderWorldState(worldMap);
+            System.out.println();
+            sleepSimulation();
+        }
+    }
+
+    public void sleepSimulation() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            System.out.println("Сон был прерван!");
+            e.printStackTrace();
+        }
     }
 }
