53ef2c24439fe9507eb6ef4a4e1b401247219959
Alexey Maryshenkov <alekseycrosso@gmail.com>
2026-04-04 00:30:59 +0300

    add reverse map for WorldMap, add some helpful methods

--- src/simulation/core/WorldMap.java
+++ src/simulation/core/WorldMap.java
@@ -1,13 +1,17 @@
 package simulation.core;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
+import simulation.entities.Creature;
 import simulation.entities.Entity;
 
 import java.util.HashMap;
 
 public class WorldMap {
     private Map<Coordinate, Entity> entities;
+    private Map<Entity, Coordinate> entityToCoordinate;
     private final int width;
     private final int height;
 
@@ -15,10 +19,7 @@ public class WorldMap {
         this.width = width;
         this.height = height;
         this.entities = new HashMap<>();
-    }
-
-    public Map<Coordinate, Entity> getEntities() {
-        return entities;
+        this.entityToCoordinate = new HashMap<>();
     }
 
     public int getWidth() {
@@ -31,9 +32,35 @@ public class WorldMap {
 
     public void placeEntity(Coordinate coordinate, Entity entity) {
         entities.put(coordinate, entity);
+        entityToCoordinate.put(entity, coordinate);
+    }
+
+    public void removeEntity(Coordinate coordinate) {
+        Entity entity = entities.remove(coordinate);
+        if (entity != null) {
+            entityToCoordinate.remove(entity);
+        }
+    }
+
+    public Coordinate getCoordinateByEntity(Entity entity) {
+        return entityToCoordinate.get(entity);
     }
 
     public Entity getEntity(Coordinate coordinate) {
         return entities.get(coordinate);
+    }
+
+    public Map<Coordinate, Entity> getEntities() {
+        return entities;
+    }
+
+    public List<Creature> getCreatures() {
+        List<Creature> creatures = new ArrayList<>();
+        for (Entity entity : entities.values()) {
+            if (entity instanceof Creature) {
+                creatures.add((Creature) entity);
+            }
+        }
+        return creatures;
     }
 }
